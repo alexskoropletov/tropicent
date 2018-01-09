4972307cecf0f5fc590e50593b63ae37f0398aeb
Alex Skoropletov <alex@skoropletov.ru>
2018-01-09 15:19:06 +0300

    - ui js codestyle updated - ui js errors output to console

--- ui/public/js/app.js
+++ ui/public/js/app.js
@@ -1,17 +1,17 @@
-$(function() {
+$(function () {
     /**
      * Когда юзер засабмиттил форму
      */
-    $('#article-helper').on('submit', function() {
+    $('#article-helper').on('submit', function () {
         // прячем кнопку, чтобы не отправлять миллион одинаковых запросов
         $('.submit-button').hide();
         // шлём запрос на контроллер ApiGateway
-        $.get('/api/keywords', {text: $('#article').val()}, function(data) {
+        $.get('/api/keywords', {text: $('#article').val()}, function (data) {
             // Если всё ОК
             if (data.result == 'OK') {
                 // формируем html-список тегов
                 var tags = [];
-                data.keywords.tags.forEach(function(item) {
+                data.keywords.tags.forEach(function (item) {
                     tags.push(
                         '<li class="list-group-item">'
                         + '<div class="checkbox">'
@@ -27,11 +27,14 @@ $(function() {
                 if (data.photo.result == 'OK') {
                     // показываем картинку и сохраняем её ключевое слово
                     showPhoto(data.photo.path, data.keywords.keyword);
+                } else {
+                    console.error('[!] Ошибка определения фото');
                 }
             }
             // вне зависимости от ответа показываем кнопку отправки формы
             $('.submit-button').show();
         }, 'json');
+        console.error('[!] Ошибка обработки данных формы');
         // на самом деле не отправляем форму никуда
         return false;
     });
@@ -62,15 +65,17 @@ function showPhoto(path, keyword) {
  */
 function photoEvent() {
     // на всякий случай снимаем все предыдущие обработчики, чтобы не сработал каскад вызовов
-    $('.photo').off('click').on('click', function() {
+    $('.photo').off('click').on('click', function () {
         // прячем картинку, чтобы с "эффектом эпилептика" не отправить миллион одинаковых запросов
         $(this).hide();
         var keyword = $(this).data('keyword');
         // шлём запрос на ApiGateway, чтобы по ключевому слову получить рандомную картинку
-        $.get('/api/photos', {keyword: keyword}, function(data) {
-             if (data.result == 'OK') {
-                 // показываем новую фотку
+        $.get('/api/photos', {keyword: keyword}, function (data) {
+            if (data.result == 'OK') {
+                // показываем новую фотку
                 showPhoto(data.response.path, keyword);
+            } else {
+                console.error('[!] Ошибка обновления фото');
             }
         }, 'json');
     });
